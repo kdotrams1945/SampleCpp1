df04d004f4670ca54b739a724bb4d95d3d2ed9c7
karth <karthikeyaramaraju@gmail.com>
2024-08-15 21:18:40 -0500

    Add project files.

---
+++ SampleCpp1/test.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "SumSolution.h"
+namespace SumTests {
+	TEST(TestCaseName, TestName) {
+		EXPECT_EQ(1, 1);
+		EXPECT_TRUE(true);
+	}
+
+	TEST(SumSolutionTest, Simple) {
+		sum3::SumSolution a;
+		vector<int> input = { -1, 0, 1, 2, -1, -4 };
+		vector<vector<int>> output = a.threeSum(input);
+		//[[-1, -1, 2], [-1, 0, 1]]
+		vector<vector<int>> wantedOutput = { {-1, -1, 2},{-1, 0, 1} };
+		EXPECT_EQ(wantedOutput, output);
+		vector<vector<int>> unwanted = { {-1, 2, 2},{-1, 0, 1} };
+		EXPECT_NE(unwanted, output);
+	}
+}

b98a0f784356a89995849bde64f7eae045491614
karth <karthikeyaramaraju@gmail.com>
2024-09-02 10:46:24 -0500

    clean up

--- SampleCpp1/sqrt.h
+++ SampleCpp1/sqrt.h
@@ -3,24 +3,20 @@ namespace leetcode_sqrt {
     class Solution {
     public:
         int mySqrt(int y) {
-           /* long x = y;
-            if (x == 1 || x == 0)
-                return x;
-            int start = 0;
-            if (x < 1000000)
-                start = x / 2;
-            else
-                start = x / 1000;
-            for (int i = start; i > 1; i--) {
-                auto z = i * i;
-                if (z > 0 && z < 2147483647 && z <= x)
-                    return i;
-            }
-            return -1;*/
-
             long x = y;
             if (x == 1 || x == 0)
                 return x;
+            auto start = getStart(x);
+            for (long i = start; i <= x; i++) {
+                if (i * i == y)
+                    return i;
+                if (i * i > x)
+                    return i - 1;
+            }
+            return -1;
+        }
+    private:
+        int getStart(long y) {
             int count = 0;
             int start = 1;
             long z = y;
@@ -31,13 +27,8 @@ namespace leetcode_sqrt {
                     start = start * 10;
                 }
             }
-            for (long i = start; i <= x; i++) {
-                if (i * i == y)
-                    return i;
-                if (i * i > x)
-                    return i - 1;
-            }
-            return -1;
+            return start;
+
         }
     };
-};
+}
